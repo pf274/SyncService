fc5c6c127973450f53b388c1464f78eda8182358
pf274 <peterfullmer33@gmail.com>
2024-08-01 13:17:55 -0600

    Restricting access to internal files

--- commands.ts
+++
@@ -1,5 +0,0 @@
-export { CreateCommand } from './src/SyncServiceBaseCommands';
-export { ReadCommand } from './src/SyncServiceBaseCommands';
-export { UpdateCommand } from './src/SyncServiceBaseCommands';
-export { DeleteCommand } from './src/SyncServiceBaseCommands';
-export { GetAllResourcesOfTypeCommand } from './src/SyncServiceBaseCommands';
--- export.ts
+++ export.ts
@@ -3,16 +3,23 @@ import { SyncService } from "./src/SyncService";
 import { CreateCommand, DeleteCommand, GetAllResourcesOfTypeCommand, ReadCommand, UpdateCommand } from "./src/SyncServiceBaseCommands";
 import { ICommand } from './src/interfaces/ICommand';
 
-export { SyncService };
-export * from './commands';
-
-export { CommandNames, ICommand }; // Re-export the CommandNames enum
-
-export const Commands = {
+export {
+  SyncService,
+  CommandNames,
+  ICommand,
   CreateCommand,
   ReadCommand,
   UpdateCommand,
   DeleteCommand,
   GetAllResourcesOfTypeCommand
 };
+
+const Commands = {
+  CreateCommand,
+  ReadCommand,
+  UpdateCommand,
+  DeleteCommand,
+  GetAllResourcesOfTypeCommand
+};
+
 export default { SyncService, Commands };
